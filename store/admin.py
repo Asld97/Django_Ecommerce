5b81fb5130e75678af564d40a0e7d104b0a94e34
unknown <robertkozak97@gmail.com>
2023-09-07 17:30:20 +0200

    Model and Admin registration progress

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from store.models import Product, Category
+from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+# Register your models here - short version
+# admin.site.register(Product)
+# admin.site.register(Category)
+
+# More flexible way
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)} # Fill in field before its spcific name - never empty field
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
+    list_filter = ['in_stock', 'is_active']
+    list_editable= ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)} # Fill in field before its spcific name - never empty field
--- store/models.py
+++ store/models.py
@@ -25,12 +25,14 @@ class Product(models.Model):
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'Products',
+        verbose_name_plural = 'products'
         ordering = ('-created',)
+        
     
     def __str__(self):
         return self.title
