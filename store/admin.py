e96d2e3bf7a33bfb307eb206573a7855177a8ceb
unknown <robertkozak97@gmail.com>
2023-09-08 18:50:00 +0200

    Test implementation - set up Coverage in requirements Gitignore update

--- store/admin.py
+++ store/admin.py
@@ -9,11 +9,11 @@ from .models import Product, Category
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)} # Fill in field before its spcific name - never empty field
+    prepopulated_fields = {'slug': ('name',)} # Fill in field before its specific data - never empty field
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
     list_filter = ['in_stock', 'is_active']
     list_editable= ['price', 'in_stock']
-    prepopulated_fields = {'slug': ('title',)} # Fill in field before its spcific name - never empty field
+    prepopulated_fields = {'slug': ('title',)} # Fill in field before its specific data - never empty field
