acc56850879e53eabcaa6f3a80a87df058a42b6f
unknown <robertkozak97@gmail.com>
2023-09-11 19:58:38 +0200

    Category layout

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'store'
+app_name = 'store' # quick reference point for url patterns below. Usefull in reverse
 
 urlpatterns = [
     path('', views.all_products, name='all_prodcuts'),
-    path('item/<slug:slug>/', views.product_detail, name='product_detail')
+    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('search/<slug:category_slug>/', views.category_list, name='category_list')
     
     
 ]
