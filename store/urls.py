6ddc40ff1b0eeeb4c99e809f01945fb4b82f78ce
unknown <robertkozak97@gmail.com>
2023-09-12 20:41:06 +0200

    Naming changes

--- store/urls.py
+++ store/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'store' # quick reference point for url patterns below. Usefull in reverse
 
 urlpatterns = [
-    path('', views.all_products, name='all_prodcuts'),
-    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('', views.product_all, name='product_all'),
+    path('<slug:slug>/', views.product_detail, name='product_detail'),
     path('search/<slug:category_slug>/', views.category_list, name='category_list')
     
     
