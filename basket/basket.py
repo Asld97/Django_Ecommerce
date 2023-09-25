0fd4a6fff514583c5868828775ebed007455fbab
unknown <robertkozak97@gmail.com>
2023-09-25 18:39:21 +0200

    Calculating sum of products in Basket

--- basket/basket.py
+++ basket/basket.py
@@ -11,11 +11,17 @@ class Basket():
         
         self.basket = basket
     
-    def add(self, product):
+    def add(self, product, qty):
         product_id = product.id
 
         if product.id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price)}
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
         
         self.session.modified = True
+    
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items"""
+
+        return sum(item['qty'] for item in self.basket.values())
             
