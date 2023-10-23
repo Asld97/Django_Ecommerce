82fdf82bd8878d698ebb16f957c8ed02bddda2b8
unknown <robertkozak97@gmail.com>
2023-10-23 21:32:38 +0200

    Template update

--- account/views.py
+++ account/views.py
@@ -7,84 +7,90 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
+from orders.views import user_orders
 
 from .forms import RegistrationForm, UserEditForm
-from .models import UserBase
+from .models import Customer
 from .tokens import account_activation_token
-from orders.views import user_orders
 
 
 @login_required
 def dashboard(request):
     orders = user_orders(request)
-    return render(request, 'account/dashboard/dashboard.html', {'orders': orders})
+    return render(request, "account/dashboard/dashboard.html", {"orders": orders})
+
 
 @login_required
-def edit_details(request):    
-    if request.method == 'POST':        
+def edit_details(request):
+    if request.method == "POST":
         user_form = UserEditForm(instance=request.user, data=request.POST)
-        
-        if user_form.is_valid():            
+
+        if user_form.is_valid():
             user_form.save()
     else:
         user_form = UserEditForm(instance=request.user)
 
-    return render(request,
-                  'account/dashboard/edit_details.html', {'user_form': user_form})
+    return render(
+        request, "account/dashboard/edit_details.html", {"user_form": user_form}
+    )
+
 
 @login_required
 def delete_user(request):
-    user = UserBase.objects.get(user_name=request.user)
+    user = Customer.objects.get(user_name=request.user)
     user.is_active = False
     user.save()
     logout(request)
-    return redirect('account:delete_confirmation')
+    return redirect("account:delete_confirmation")
+
 
 # Create your views here.
 def account_register(request):
-
     if request.user.is_authenticated:
-        return redirect('account:dashboard')
+        return redirect("account:dashboard")
 
-    if request.method == 'POST':
+    if request.method == "POST":
         registerForm = RegistrationForm(request.POST)
         if registerForm.is_valid():
             user = registerForm.save(commit=False)
-            user.email = registerForm.cleaned_data['email']
-            user.set_password(registerForm.cleaned_data['password'])
+            user.email = registerForm.cleaned_data["email"]
+            user.set_password(registerForm.cleaned_data["password"])
             user.is_active = False
             user.save()
             current_site = get_current_site(request)
-            
-            subject = 'Activate your Account'
-            message = render_to_string('account/registration/account_activation_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
 
-            
+            subject = "Activate your Account"
+            message = render_to_string(
+                "account/registration/account_activation_email.html",
+                {
+                    "user": user,
+                    "domain": current_site.domain,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": account_activation_token.make_token(user),
+                },
+            )
+
             user.email_user(subject=subject, message=message)
-            return render(request, 'account/registration/register_email_confirm.html', {'form': registerForm})
+            return render(
+                request,
+                "account/registration/register_email_confirm.html",
+                {"form": registerForm},
+            )
     else:
         registerForm = RegistrationForm()
-    return render(request, 'account/registration/register.html', {'form': registerForm})
-    
+    return render(request, "account/registration/register.html", {"form": registerForm})
+
 
 def account_activate(request, uidb64, token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
-        user = UserBase.objects.get(pk=uid)
-    except():
+        user = Customer.objects.get(pk=uid)
+    except ():
         pass
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
         login(request, user)
-        return redirect('account:dashboard')
+        return redirect("account:dashboard")
     else:
-        return render(request, 'account/registration/activation_invalid.html')
-
-
-
+        return render(request, "account/registration/activation_invalid.html")
