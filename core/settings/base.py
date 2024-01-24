257de19c6ab69531fa032e792f15d58e2387c44f
unknown <robertkozak97@gmail.com>
2024-01-24 19:35:14 +0100

    Random Secret Key

--- core/settings/base.py
+++ core/settings/base.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 
 from decouple import config
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -24,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = get_random_secret_key()
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
